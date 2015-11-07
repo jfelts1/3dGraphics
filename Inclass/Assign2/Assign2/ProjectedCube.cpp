6094a9a4738b6e001c0df9f6447bfc5f6e1104f8
James Felts <jfelts1@users.noreply.github.com>
2015-11-06 16:44:49 -0800

    crazy lambda template toggle thingy completed

--- Inclass/Assign2/Assign2/ProjectedCube.cpp
+++ Inclass/Assign2/Assign2/ProjectedCube.cpp
@@ -7,7 +7,9 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
+#include <functional>
+#include <cmath>
 
 #define GLM_FORCE_RADIANS 
 #define GLM_FORCE_CXX11
@@ -15,7 +17,13 @@
 #include <glm/mat4x4.hpp>
 #include <glm/gtc/matrix_transform.hpp> 
 #include "Utils/ShaderUtils.h"
+#include "Toggle.h"
+#include "cubedata.h"
 
+void init();
+void display(void);
+void Reshape(int width, int height);
+void keyboard(unsigned char key, int x, int y);
 
 GLuint cube_vao;
 GLuint cube_vbo;
@@ -38,82 +46,78 @@ static const double kPI = 3.1415926535897932384626433832795;
 glm::mat4 view_matrix;
 glm::mat4 projection_matrix;
 using namespace glm;
+using std::function;
 
 float aspect = 0.0;
 
-GLfloat eye[3] = { 0.0f, 0.0f, 2.0f };
+vec3 eye(0.0f,0.0f,2.0f);
+vec3 center(0.0f);
+//GLfloat center[3] = { 0.0f, 0.0f, 0.0f };
 
-GLfloat center[3] = { 0.0f, 0.0f, 0.0f };
 
 GLfloat zNear = 1.5f;
 GLfloat zFar = 10.0f;
 
-static const GLfloat cube_vertices[] = {
-0.5f, 0.5f, 0.5f, 1.0f,  // v0,v1,v2,v3 (front)
--0.5f, 0.5f, 0.5f, 1.0f,
--0.5f, -0.5f, 0.5f, 1.0f,
-0.5f, -0.5f, 0.5f, 1.0f,
-0.5f, 0.5f, 0.5f, 1.0f,  // v0,v3,v4,v5 (right)
-0.5f, -0.5f, 0.5f, 1.0f,
-0.5f, -0.5f, -0.5f, 1.0f,
-0.5f, 0.5f, -0.5f, 1.0f,
-0.5f, 0.5f, 0.5f, 1.0f,  // v0,v5,v6,v1 (top)
-0.5f, 0.5f, -0.5f, 1.0f,
--0.5f, 0.5f, -0.5f, 1.0f,
--0.5f, 0.5f, 0.5f, 1.0f,
--0.5f, 0.5f, 0.5f, 1.0f, // v1,v6,v7,v2 (left)
--0.5f, 0.5f, -0.5f, 1.0f,
--0.5f, -0.5f, -0.5f, 1.0f,
--0.5f, -0.5f, 0.5f, 1.0f,
--0.5f, -0.5f, -0.5f, 1.0f,// v7,v4,v3,v2 (bottom)
-0.5f, -0.5f, -0.5f, 1.0f,
-0.5f, -0.5f, 0.5f, 1.0f,
--0.5f, -0.5f, 0.5f, 1.0f,
-0.5f, -0.5f, -0.5f, 1.0f,// v4,v7,v6,v5 (back)
--0.5f, -0.5f, -0.5f, 1.0f,
--0.5f, 0.5f, -0.5f, 1.0f,
-0.5f, 0.5f, -0.5f, 1.0f };
+// Choose whether the back face will be enabled or not
+auto cullToggle = Toggle<function<void()>,function<void()>,void>(
+[]()
+{
+    std::cout<<"Cull enabled"<<std::endl;
+    glEnable(GL_CULL_FACE);
+}
+,
+[]()
+{
+    std::cout<<"Cull disabled"<<std::endl;
+    glDisable(GL_CULL_FACE);
+});
 
+// Choose whether to draw in wireframe mode or not
+auto lineToggle = Toggle<function<void()>,function<void()>,void>(
+[]()
+{
+    std::cout<<"Line enabled"<<std::endl;
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+}
+,
+[]()
+{
+    std::cout<<"Line disabled"<<std::endl;
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+});
 
-static const GLfloat cube_normals[] = {
-0.0f, 0.0f, 1.0f, // v0,v1,v2,v3 (front)
-0.0f, 0.0f, 1.0f,
-0.0f, 0.0f, 1.0f,
-0.0f, 0.0f, 1.0f,
-1.0f, 0.0f, 0.0f, // v0,v3,v4,v5 (right)
-1.0f, 0.0f, 0.0f,
-1.0f, 0.0f, 0.0f,
-1.0f, 0.0f, 0.0f,
-0.0f, 1.0f, 0.0f, // v0,v5,v6,v1 (top)
-0.0f, 1.0f, 0.0f,
-0.0f, 1.0f, 0.0f,
-0.0f, 1.0f, 0.0f,
-1.0f, 0.0f, 0.0f,// v1,v6,v7,v2 (left)
-1.0f, 0.0f, 0.0f,
-1.0f, 0.0f, 0.0f,
-1.0f, 0.0f, 0.0f,
-0.0f, 1.0f, 0.0f, // v7,v4,v3,v2 (bottom)
-0.0f, 1.0f, 0.0f,
-0.0f, 1.0f, 0.0f,
-0.0f, 1.0f, 0.0f,
-0.0f, 0.0f, 1.0f, // v4,v7,v6,v5 (back)
-0.0f, 0.0f, 1.0f,
-0.0f, 0.0f, 1.0f,
-0.0f, 0.0f, 1.0f };
+// Choose the type of Projection
+auto orthographicToggle = Toggle<function<glm::mat4(GLfloat,GLfloat)>,function<glm::mat4 (GLfloat,GLfloat)>,glm::mat4>(
+[&](GLfloat zNear, GLfloat zFar)
+{
+    std::cout<<"Orthographic disabled"<<std::endl;
+    return frustum(-1.0f, 1.0f, -1.0f, 1.0f, zNear, zFar);
+}
+,
+[&](GLfloat zNear, GLfloat zFar)
+{
+    std::cout<<"Orthographic enabled"<<std::endl;
+    return ortho(-1.0f, 1.0f, -1.0f, 1.0f, zNear, zFar);
+});
 
-
-static const GLushort cube_indices[] = {
-0, 1, 2, 2, 3, 0,      // front
-4, 5, 6, 6, 7, 4,      // right
-8, 9, 10, 10, 11, 8,      // top
-12, 13, 14, 14, 15, 12,      // left
-16, 17, 18, 18, 19, 16,      // bottom
-20, 21, 22, 22, 23, 20 };    // back
-
-void init();
-void display(void);
-void Reshape(int width, int height);
-void keyboard(unsigned char key, int x, int y);
+auto isometricToggle = Toggle<function<glm::mat4(vec3,vec3)>,function<glm::mat4(vec3,vec3)>,glm::mat4>(
+[&](vec3 eye,vec3 center)
+{
+    std::cout<<"Isometric enabled"<<std::endl;
+   // mat3 tmp(\
+             sqrt(3.0f),0.0f,-sqrt(3.0f),\
+             1.0f,2.0f,1.0f,\
+             sqrt(2.0f),-sqrt(2.0f),sqrt(2.0f));
+   // tmp*=(1.0f/sqrt(6.0f));
+   // vec3 eyeTmp = tmp*eye;
+    return glm::lookAt(eye,center,vec3(1.0f));
+}
+,
+[&](vec3 eye, vec3 center)
+{
+    std::cout<<"Isometric disabled"<<std::endl;
+    return glm::lookAt(eye,center,vec3(-1.0f));
+});
 
 void Initialize(void)
 {
@@ -157,6 +161,12 @@ void Initialize(void)
 	glEnableVertexAttribArray(normal_loc);
 
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+    //init draw
+    projection_matrix = orthographicToggle.toggle(zNear,zFar);
+    lineToggle.toggle();
+    cullToggle.toggle();
+    projection_matrix = isometricToggle.toggle(eye,center);
 }
 
 
@@ -169,48 +179,15 @@ void Display(void)
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LEQUAL);
 
-	// Choose whether the back face will be enabled or not
-
-	if (cullface)
-    {
-		glEnable(GL_CULL_FACE);
-    }
-	else
-    {
-		glDisable(GL_CULL_FACE);
-    }
-
-	// Choose whether to draw in wireframe mode or not
-
-	if (show_line)
-    {
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    }
-	else
-    {
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    }
-
 
 	// Setup view matrix
 
-
-	view_matrix = lookAt(vec3(eye[0], eye[1], eye[2]), vec3(center[0], center[1], center[2]), vec3(0.0f, 1.0f, 0.0f)); 
+    view_matrix = lookAt(eye, center, vec3(0.0f, 1.0f, 0.0f));
 
 	glUniformMatrix4fv(static_cast<GLint>(view_matrix_loc), 1, GL_FALSE, reinterpret_cast<GLfloat*>(&view_matrix[0]));
 
 
 	// Choose the type of Projection
-
-
-	if (orthographic)
-	{
-		projection_matrix = ortho(-1.0f, 1.0f, -1.0f, 1.0f, zNear, zFar);
-	}
-	else
-	{
-		projection_matrix = frustum(-1.0f, 1.0f, -1.0f, 1.0f, zNear, zFar);
-	}
 
     glUniformMatrix4fv(
                 static_cast<GLint>(projection_matrix_loc), 1, GL_FALSE, reinterpret_cast<GLfloat*>(&projection_matrix[0]));
@@ -241,17 +218,26 @@ void keyboard(unsigned char key, int x, int y){
 		break;
 
 	case 'o':
-		orthographic = !orthographic;
+        projection_matrix = orthographicToggle.toggle(zNear,zFar);
 		break;
 	case 's':
-		show_line = !show_line;
+        lineToggle.toggle();
 		break;
 	case 'c':
-		cullface = !cullface;
+        cullToggle.toggle();
 		break;
 	
 	case 'i':
 		// implement isometric view
+        projection_matrix = isometricToggle.toggle(eye,center);
+        for(int i =0;i<3;i++)
+        {
+            for(int j =0;j<3;j++)
+            {
+                printf("%f ",projection_matrix[i][j]);
+            }
+            printf("\n");
+        }
 		break;
 	case 'd':
 		//implement dimetric view
--- Inclass/Assign2/Assign2/Toggle.h
+++ Inclass/Assign2/Assign2/Toggle.h
@@ -1,12 +1,40 @@
 #ifndef TOGGLE_H
 #define TOGGLE_H
+#include <functional>
+#include <iostream>
 
-
+template<typename func1,typename func2,typename ret>
 class Toggle
 {
 public:
-    Toggle();
+    Toggle()=default;
+    Toggle(func1 f1,func2 f2)
+    {
+        m_func1 = f1;
+        m_func2 = f2;
+        m_curFunc = 1;
+    }
+    ~Toggle()=default;
+    template<typename... args>
+    ret toggle(args... ar)
+    {
+        if(m_curFunc == 1)
+        {
+            m_curFunc = 2;
+            return m_func2(ar...);
+        }
+        else
+        {
+            m_curFunc = 1;
+            return m_func1(ar...);
+        }
+    }
 
+private:
+
+    func1 m_func1;
+    func2 m_func2;
+    int m_curFunc;
 };
 
 #endif // TOGGLE_H
