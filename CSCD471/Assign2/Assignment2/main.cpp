ebcec5f107e0fcb0452dc7502ac1ab3822d9b016
James Felts <jfelts1@users.noreply.github.com>
2016-01-22 01:55:20 -0800

    bunny now deforms

--- CSCD471/Assign2/Assignment2/main.cpp
+++ CSCD471/Assign2/Assignment2/main.cpp
@@ -10,13 +10,17 @@
 
 #define GLM_FORCE_RADIANS 
 #define GLM_FORCE_CXX11
-#define TWOPI 2*3.1415926535897932384626433832795
+#define TWOPI 2*3.1415926535897932384626433832795f
+#define WAVE_AMPLITUDE 0.08f
+#define WAVE_FREQUENCY 1.0f
+#define WAVE_PHASE 0.0f
 
 #include <glm/mat4x4.hpp> // glm::mat4
 #include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
 
 #include "objloader.h"
 #include "shader.h"
+#include "model_deform.h"
 
 
 using namespace glm;
@@ -53,7 +57,7 @@ GLfloat timeUpdate = 0.0;
 
 
 void Initialize();
-void display(void);
+void Display(void);
 void makeIdentity();
 void Reshape(int width, int height);
 void glutMouse(int button, int state, int x, int y);
@@ -150,9 +154,11 @@ void Display(void)
 	updateTime();
 	
 	glBindVertexArray(vao);
-
-	 //Here you need to update vertices using either glMapBuffer of glBufferSubData
-
+	//Here you need to update vertices using either glMapBuffer of glBufferSubData
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	float defAmount = WAVE_AMPLITUDE*sin(TWOPI * WAVE_FREQUENCY*timeUpdate + WAVE_PHASE);
+	deformModel(vertices, normals, defAmount);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, vertices.size()*sizeof(decltype(vertices[0])), vertices.data());
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 	
@@ -162,7 +168,7 @@ void Display(void)
 	glUniformMatrix4fv(glGetUniformLocation(program, "ProjectionMatrix"), 1, GL_FALSE, reinterpret_cast<GLfloat*>(&projection[0]));
 
 
-	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, nullptr);
+	glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(indices.size()), GL_UNSIGNED_INT, nullptr);
 	
 	glBindVertexArray(0);
 
@@ -197,6 +203,10 @@ void updateTime(){
 
 /**************************************************************************************/
 // ReSharper disable CppParameterNeverUsed
+#ifdef WIN32
+#pragma warning(push)
+#pragma warning(disable: 4100)
+#endif
 void keyboard(unsigned char key, int x, int y)
 {
 	// ReSharper restore CppParameterNeverUsed
@@ -213,6 +223,9 @@ void keyboard(unsigned char key, int x, int y)
 
 	glutPostRedisplay();
 }
+#ifdef WIN32
+#pragma warning(pop)
+#endif
 
 /*********************************************************************************************/
 void makeIdentity(){
@@ -292,13 +305,13 @@ void glutMotion(int x, int y)
 		vec3 lastPos;
 		vec3 currPos;
 
-		lastPos.x = gLastMouseX * 2.0 / gViewportWidth - 1.0;
-		lastPos.y = (gViewportHeight - gLastMouseY) * 2.0 / gViewportHeight - 1.0;
-		lastPos.z = projectToTrackball(kTrackBallRadius, lastPos[0], lastPos[1]);
+		lastPos.x = gLastMouseX * 2.0f / gViewportWidth - 1.0f;
+		lastPos.y = (gViewportHeight - gLastMouseY) * 2.0f / gViewportHeight - 1.0f;
+		lastPos.z = static_cast<float>(projectToTrackball(kTrackBallRadius, lastPos[0], lastPos[1]));
 
-		currPos.x = x * 2.0 / gViewportWidth - 1.0;
-		currPos.y = (gViewportHeight - y) * 2.0 / gViewportHeight - 1.0;
-		currPos.z = projectToTrackball(kTrackBallRadius, currPos[0], currPos[1]);
+		currPos.x = x * 2.0f / gViewportWidth - 1.0f;
+		currPos.y = (gViewportHeight - y) * 2.0f / gViewportHeight - 1.0f;
+		currPos.z = static_cast<float>(projectToTrackball(kTrackBallRadius, currPos[0], currPos[1]));
 		
 		currPos = normalize(currPos);
 		lastPos = normalize(lastPos);
@@ -315,17 +328,17 @@ void glutMotion(int x, int y)
 
 		mat4 deltaRotation(1.0f);
 
-		deltaRotation[0][0] = cos_rotate_angle + rotateVecCross[0] * rotateVecCross[0] * (1 - cos_rotate_angle);
-		deltaRotation[0][1] = rotateVecCross[0] * rotateVecCross[1] * (1 - cos_rotate_angle) + rotateVecCross[2] * sin_rotate_angle;
-		deltaRotation[0][2] = rotateVecCross[0] * rotateVecCross[2] * (1 - cos_rotate_angle) - rotateVecCross[1] * sin_rotate_angle;
-		deltaRotation[0][3] = 0.0;
-		deltaRotation[1][0] = rotateVecCross[0] * rotateVecCross[1] * (1 - cos_rotate_angle) - rotateVecCross[2] * sin_rotate_angle;
-		deltaRotation[1][1] = cos_rotate_angle + rotateVecCross[1] * rotateVecCross[1] * (1 - cos_rotate_angle);
-		deltaRotation[1][2] = rotateVecCross[2] * rotateVecCross[1] * (1 - cos_rotate_angle) + rotateVecCross[0] * sin_rotate_angle;
-		deltaRotation[1][3] = 0.0;
-		deltaRotation[2][0] = rotateVecCross[0] * rotateVecCross[2] * (1 - cos_rotate_angle) + rotateVecCross[1] * sin_rotate_angle;
-		deltaRotation[2][1] = rotateVecCross[1] * rotateVecCross[2] * (1 - cos_rotate_angle) - rotateVecCross[0] * sin_rotate_angle;
-		deltaRotation[2][2] = cos_rotate_angle + rotateVecCross[2] * rotateVecCross[2] * (1 - cos_rotate_angle);
+		deltaRotation[0][0] = static_cast<float>(cos_rotate_angle + rotateVecCross[0] * rotateVecCross[0] * (1 - cos_rotate_angle));
+		deltaRotation[0][1] = static_cast<float>(rotateVecCross[0] * rotateVecCross[1] * (1 - cos_rotate_angle) + rotateVecCross[2] * sin_rotate_angle);
+		deltaRotation[0][2] = static_cast<float>(rotateVecCross[0] * rotateVecCross[2] * (1 - cos_rotate_angle) - rotateVecCross[1] * sin_rotate_angle);
+		deltaRotation[0][3] = 0.0f;
+		deltaRotation[1][0] = (float)(rotateVecCross[0] * rotateVecCross[1] * (1 - cos_rotate_angle) - rotateVecCross[2] * sin_rotate_angle);
+		deltaRotation[1][1] = static_cast<float>(cos_rotate_angle + rotateVecCross[1] * rotateVecCross[1] * (1 - cos_rotate_angle));
+		deltaRotation[1][2] = static_cast<float>(rotateVecCross[2] * rotateVecCross[1] * (1 - cos_rotate_angle) + rotateVecCross[0] * sin_rotate_angle);
+		deltaRotation[1][3] = 0.0f;
+		deltaRotation[2][0] = static_cast<float>(rotateVecCross[0] * rotateVecCross[2] * (1 - cos_rotate_angle) + rotateVecCross[1] * sin_rotate_angle);
+		deltaRotation[2][1] = static_cast<float>(rotateVecCross[1] * rotateVecCross[2] * (1 - cos_rotate_angle) - rotateVecCross[0] * sin_rotate_angle);
+		deltaRotation[2][2] = static_cast<float>(cos_rotate_angle + rotateVecCross[2] * rotateVecCross[2] * (1 - cos_rotate_angle));
 		
 
 		mat4 tempMatrix;
@@ -344,8 +357,8 @@ void glutMotion(int x, int y)
 	}
 	else if (gIsScalingCamera)
 	{
-		float y1 = gViewportHeight - gLastMouseY;
-		float y2 = gViewportHeight - y;
+		float y1 = static_cast<float>(gViewportHeight - gLastMouseY);
+		float y2 = static_cast<float>(gViewportHeight - y);
 
 		gCameraScale *= 1 + (y1 - y2) / gViewportHeight;
 	}
