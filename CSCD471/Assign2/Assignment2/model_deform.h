626f69b038de504f377da97eea742fef8df76da0
James Felts <jfelts1@users.noreply.github.com>
2016-01-22 12:54:35 -0800

    finished Assign2

--- CSCD471/Assign2/Assignment2/model_deform.h
+++ CSCD471/Assign2/Assignment2/model_deform.h
@@ -3,7 +3,6 @@
 
 inline std::vector<glm::vec3> deformModel(std::vector<glm::vec3> vertices, std::vector<glm::vec3>& normals,const float amount)
 {
-	
 	for (size_t i = 0;i < vertices.size();i++)
 	{
 		vertices[i] += amount*normals[i];
